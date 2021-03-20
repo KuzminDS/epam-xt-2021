13447d8d036b21252c3c339d3a822beb193e785f
Dmitriy <dima251200@yandex.ru>
2021-03-20 21:10:00 +0400

    Took out enums

---
+++ Task 2/Task 2.1/Task 2.1.2/CustomPaint/Entities/Color.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPaint.Entities
+{
+    public enum Color 
+    { 
+        Yellow = 1, 
+        Orage, 
+        Red, 
+        Green, 
+        Blue, 
+        Purple, 
+        Black 
+    }
+}
---
+++ Task 2/Task 2.1/Task 2.1.2/CustomPaint/Shapes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPaint
+{
+    enum Shapes
+    {
+        None,
+        Point,
+        Line,
+        Circle,
+        Ring,
+        Triangle,
+        Square,
+        Rectangle
+    }
+}
