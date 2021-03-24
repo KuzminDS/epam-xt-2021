d0493cb0362ca032fc506a94dd647097a68c2a3f
Dmitriy <dima251200@yandex.ru>
2021-03-24 17:08:41 +0400

    Added Task 3.1

---
+++ Task 3/Task 3.1/Task 3.1.1/WeakestLink/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeakestLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВЫВОД: Введите N");
+            var number = EnterNumber();
+            Console.WriteLine("ВЫВОД: Введите, какой по счету человек будет вычеркнут каждый раунд:");
+            var removingNumber = EnterNumber();
+
+            var people = CreateList(number).ToList();
+
+            if (number >= removingNumber)
+                StartGame(people, removingNumber);
+            else
+                Console.WriteLine("ВЫВОД: Невозможно вычеркнуть человека с номером больше, чем есть в кругу.");
+        }
+
+        private static void StartGame(ICollection<Person> people, int removingNumber)
+        {
+            Console.WriteLine("ВЫВОД: Сгенерирован круг людей. Начинаем вычеркивать каждого второго.");
+
+            var removingIndex = removingNumber - 1;
+            var round = 1;
+
+            while (people.Count >= removingNumber)
+            {
+                var person = people.ElementAtOrDefault(removingIndex);
+
+                if (person != null)
+                {
+                    people.Remove(person);
+                    Console.WriteLine($"ВЫВОД: Раунд {round++}. Вычеркнут человек. Людей осталось: {people.Count}");
+
+                    removingIndex += removingNumber - 1;
+                }
+                else
+                {
+                    removingIndex = removingNumber - 1 - people.Count + removingIndex;
+                }
+            }
+
+            Console.WriteLine("ВЫВОД: Игра окончена. Невозможно вычеркнуть больше людей.");
+        }
+
+        private static IEnumerable<Person> CreateList(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                yield return new Person { Number = i };
+            }
+        }
+
+        private static int EnterNumber()
+        {
+            do
+            {
+                Console.Write("ВВОД: ");
+                var value = Console.ReadLine();
+                if (int.TryParse(value, out int number) && number > 0)
+                    return number;
+                else
+                    Console.WriteLine("ВЫВОД: Неправильное значение! Введите целочисленное положительное число.");
+
+            } while (true);
+        }
+    }
+}
---
+++ Task 3/Task 3.1/Task 3.1.2/TextAnalysis/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя файла:");
+            var fileName = Console.ReadLine();
+            string text;
+
+            try
+            {
+                text = ReadFile(fileName);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Файл с указанным путем не может быть считан.");
+                return;
+            }
+
+            var table = GetWordsFrequency(text);
+
+            Console.WriteLine("Частота слов в тексте:");
+            foreach (var item in table)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+
+        private static IDictionary<string, int> GetWordsFrequency(string text)
+        {
+            var table = new Dictionary<string, int>();
+            var separators = "[} .,;:!?(){]\"'" + Environment.NewLine;
+            var words = text.Split(separators.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                var lowercaseWord = word.ToLower();
+                if (table.ContainsKey(lowercaseWord))
+                {
+                    table[lowercaseWord]++;
+                }
+                else
+                {
+                    table.Add(lowercaseWord, 1);
+                }
+            }
+
+            return table.OrderByDescending(pair => pair.Value)
+                        .ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+
+        private static string ReadFile(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
